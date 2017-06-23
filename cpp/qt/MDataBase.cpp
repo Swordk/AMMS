fd15f4e5e9637d9df5db72725c3f7f5dbad311ea
wangyijian <wangyijian0525@gmail.com>
2017-06-23 18:16:25 +0800

    [ADD] 增加 MDB

---
+++ cpp/qt/MDataBase.cpp
@@ -0,0 +1,105 @@
+﻿/////////////////////////////////////////////////////////////////////////﻿
+///MDataBase
+///20160919 jason wong 创建该文件
+///Movie 数据库
+/////////////////////////////////////////////////////////////////////////
+
+#include "MDataBase.h"
+#include <fstream>
+#include <sstream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+namespace amms {
+    class CJsonFileHandler {
+    public:
+        CJsonFileHandler(){}
+        ~CJsonFileHandler(){}
+
+    public:
+        static std::string LoadFile(const std::string& strFileName) {
+            std::ifstream ifs(strFileName);
+            std::stringstream ssBuffer;
+            ssBuffer << ifs.rdbuf();
+            return ssBuffer.str();
+        }
+
+        //
+        static bool LoadKey2List(const std::string& i_strFileName,
+            std::map<std::string, std::set<std::string> >& o_mapData) {
+            std::string strContents = LoadFile(i_strFileName);
+            rapidjson::Document doc;
+            doc.Parse(strContents.c_str());
+            if (doc.IsObject() == false)
+                return false;
+
+            o_mapData.clear();
+            for (auto itr = doc.MemberBegin(); itr != doc.MemberEnd(); ++itr) {
+                std::string strKey = itr->name.GetString();
+                for (auto& itValue : itr->value.GetArray()) {
+                    std::string strValue = itValue.GetString();
+                    o_mapData[strKey].insert(strValue);
+                }
+            }
+            return true;
+        }
+
+        //
+        static bool LoadKey2Key2List(const std::string& i_strFileName,
+            std::map<std::string, std::map<std::string, std::set<std::string> > >& o_mapData) {
+            std::string strContents = LoadFile(i_strFileName);
+            rapidjson::Document doc;
+            doc.Parse(strContents.c_str());
+            if (doc.IsObject() == false)
+                return false;
+
+            o_mapData.clear();
+            for (auto itKey1 = doc.MemberBegin(); itKey1 != doc.MemberEnd(); ++itKey1) {
+                std::string strKey1 = itKey1->name.GetString();
+                for (auto itKey2 = itKey1->value.MemberBegin(); itKey2 != itKey1->value.MemberEnd(); ++itKey2) {
+                    std::string strKey2 = itKey2->name.GetString();
+                    for (auto& itValue : itKey2->value.GetArray()) {
+                        o_mapData[strKey1][strKey2].insert(itValue.GetString());
+                    }
+                }
+            }
+            return true;
+        }
+    };
+
+    #define LoadActors2Sn(mapActors2Sn)         CJsonFileHandler::LoadKey2List("database/actors_2_sn.json", mapActors2Sn)
+    #define LoadGenres2Sn(mapGenres2Sn)         CJsonFileHandler::LoadKey2List("database/genres_2_sn.json", mapGenres2Sn)
+    #define LoadProducers2Sn(mapProducers2Sn)   CJsonFileHandler::LoadKey2List("database/producers_2_sn.json", mapProducers2Sn)
+    #define LoadPublisher2Sn(mapPublisher2Sn)   CJsonFileHandler::LoadKey2List("database/publisher_2_sn.json", mapPublisher2Sn)
+    #define LoadS2Sn(mapS2Sn)                   CJsonFileHandler::LoadKey2List("database/S_2_sn.json", mapS2Sn)
+    #define LaodSeries2Sn(mapSeries2Sn)         CJsonFileHandler::LoadKey2List("database/series_2_sn.json", mapSeries2Sn)
+    #define LoadMovies(mapMovies)               CJsonFileHandler::LoadKey2Key2List("database/movies.json", mapMovies)
+}
+
+
+namespace amms {
+    CMDataBase* CMDataBase::GetInstance()
+    {
+        static CMDataBase s_instance;
+        return &s_instance;
+    }
+
+    void CMDataBase::Init()
+    {
+        bool bRtn = LoadActors2Sn(mapActors2Sn);
+
+        bRtn = LoadGenres2Sn(mapGenres2Sn);
+
+        bRtn = LoadProducers2Sn(mapProducers2Sn);
+
+        bRtn = LoadPublisher2Sn(mapPublisher2Sn);
+
+        bRtn = LoadS2Sn(mapS2Sn);
+
+        bRtn = LaodSeries2Sn(mapSeries2Sn);
+
+        bRtn = LoadMovies(mapMovies);
+    }
+
+}
---
+++ cpp/qt/MDataBase.h
@@ -0,0 +1,97 @@
+﻿/////////////////////////////////////////////////////////////////////////﻿
+///MDataBase
+///20160919 jason wong 创建该文件
+///Movie 数据库
+/////////////////////////////////////////////////////////////////////////
+
+#pragma once
+#include <string>
+#include <map>
+#include <set>
+
+namespace amms {
+
+    class CMDataBase {
+    public:
+        static CMDataBase* GetInstance();
+        void Init();
+
+        inline std::map<std::string, std::set<std::string> > Actors2Sn() const {
+            return mapActors2Sn;
+        }
+        inline std::set<std::string> Actors2Sn(const std::string& strActor) const {
+            if (mapActors2Sn.count(strActor) > 0)
+                return mapActors2Sn.at(strActor);
+            return std::set<std::string>();
+        }
+
+        inline std::map<std::string, std::set<std::string> > Genres2Sn() const {
+            return mapGenres2Sn;
+        }
+        inline std::set<std::string> Genres2Sn(const std::string& strGenres) const {
+            if (mapGenres2Sn.count(strGenres) > 0)
+                return mapGenres2Sn.at(strGenres);
+            return std::set<std::string>();
+        }
+
+        inline std::map<std::string, std::set<std::string> > Producers2Sn() const {
+            return mapProducers2Sn;
+        }
+        inline std::set<std::string> Producers2Sn(const std::string& strProducer) const {
+            if (mapProducers2Sn.count(strProducer))
+                return mapProducers2Sn.at(strProducer);
+            return std::set<std::string>();
+        }
+
+        inline std::map<std::string, std::set<std::string> > Publisher2Sn() const {
+            return mapPublisher2Sn;
+        }
+        inline std::set<std::string> Publisher2Sn(const std::string& strPublisher) const {
+            if (mapPublisher2Sn.count(strPublisher))
+                return mapPublisher2Sn.at(strPublisher);
+            return std::set<std::string>();
+        }
+
+        inline std::map<std::string, std::set<std::string> > S2Sn() const {
+            return mapS2Sn;
+        }
+        inline std::set<std::string> S2Sn(const std::string& strS) const {
+            if (mapS2Sn.count(strS))
+                return mapS2Sn.at(strS);
+            return std::set<std::string>();
+        }
+
+        inline std::map<std::string, std::set<std::string> > Series2Sn() const {
+            return mapSeries2Sn;
+        }
+
+        inline std::set<std::string> Series2Sn(const std::string& strSeries) const {
+            if (mapSeries2Sn.count(strSeries))
+                return mapSeries2Sn.at(strSeries);
+            return std::set<std::string>();
+        }
+
+        inline std::map<std::string, std::map<std::string, std::set<std::string> > > Movies() const {
+            return mapMovies;
+        }
+        inline std::map<std::string, std::set<std::string> > Movies(const std::string& strSn) const {
+            if (mapMovies.count(strSn))
+                return mapMovies.at(strSn);
+            return std::map<std::string, std::set<std::string> >();
+        }
+
+    private:
+        CMDataBase()    {}
+
+        std::map<std::string, std::set<std::string> > mapActors2Sn;
+        std::map<std::string, std::set<std::string> > mapGenres2Sn;
+        std::map<std::string, std::set<std::string> > mapProducers2Sn;
+        std::map<std::string, std::set<std::string> > mapPublisher2Sn;
+        std::map<std::string, std::set<std::string> > mapS2Sn;
+        std::map<std::string, std::set<std::string> > mapSeries2Sn;
+        // sn->item->content
+        std::map<std::string, std::map<std::string, std::set<std::string> > > mapMovies;
+    };
+
+#define MDB CMDataBase::GetInstance
+}
