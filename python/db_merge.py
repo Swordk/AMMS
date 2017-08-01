e39eb73ce40393644d2e181bed42bfbf17ffce10
wang.yijian <wangyijian0525@gmail.com>
2017-08-01 22:44:03 +0800

    [MOD] 合并； 电影目录处理

--- python/db_merge.py
+++ python/db_merge.py
@@ -24,24 +24,24 @@ def dict_not_in(dict1, dict2):
 
 def movies_merge():
     str_movie1_path = 'D:\\Program\\amms\\database\\movies_info.json'
-    str_movie2_path = 'python\\movies.json'
+    str_movie2_path = 'movies.json'
 
 
     dict_movies_info_all = {}
     with codecs.open(str_movie1_path, 'r', 'utf-8') as f:
         dict_movies_info_all = json.load(f)
 
-    list_movies_new = []
+    dict_movies_new = []
     with codecs.open(str_movie2_path, 'r', 'utf-8') as f:
-        list_movies_new = json.load(f)
+        dict_movies_new = json.load(f)
 
-    dict_movies_new = {}
-    for list1 in list_movies_new:
-        if len(list1) != 2:
-            print('key 2 value error')
-            print(list1)
-            continue
-        dict_movies_new[list1[0]] = list1[1]
+    # dict_movies_new = {}
+    # for list1 in dict_movies_new:
+    #     if len(list1) != 2:
+    #         print('key 2 value error')
+    #         print(list1)
+    #         continue
+    #     dict_movies_new[list1[0]] = list1[1]
     
     dict_movies_info_all.update(dict_movies_new)
     movies_json = json.dumps(dict_movies_info_all, ensure_ascii=False, indent=2)
@@ -49,38 +49,38 @@ def movies_merge():
         f.write(movies_json)
 
 
-# movies_merge()
+movies_merge()
 def actors_new():
     str_actor1_path = 'D:\\Program\\amms\\database\\actors_info.json'
-    str_actor2_path = 'python\\actors.json'
+    str_actor2_path = 'actors.json'
 
     dict_actors_info_all = {}
     with codecs.open(str_actor1_path, 'r', 'utf-8') as f:
         dict_actors_info_all = json.load(f)
 
-    list_actors_info_new_all = []
+    dict_actors_info_new = []
     with codecs.open(str_actor2_path, 'r', 'utf-8') as f:
-        list_actors_info_new_all = json.load(f)
+        dict_actors_info_new = json.load(f)
     
-    dict_actors_info_new = {}
-    for list1 in list_actors_info_new_all:
-        if len(list1) != 2:
-            print('key 2 value error')
-            print(list1)
-            continue
-        if list1[0] not in dict_actors_info_all.keys():
-            dict_actors_info_new[list1[0]] = list1[1]
+    # dict_actors_info_new = {}
+    # for list1 in dict_actors_info_new:
+    #     if len(list1) != 2:
+    #         print('key 2 value error')
+    #         print(list1)
+    #         continue
+    #     if list1[0] not in dict_actors_info_all.keys():
+    #         dict_actors_info_new[list1[0]] = list1[1]
     
     actors_json_new = json.dumps(dict_actors_info_new, ensure_ascii=False, indent=2)
     with codecs.open('actors_new.json', 'w', 'utf-8') as f:
         f.write(actors_json_new)
 
-# actors_new()
+actors_new()
 
 
 def actors_merge():
     str_actor1_path = 'D:\\Program\\amms\\database\\actors_info.json'
-    str_actor2_path = 'actors_info.json'
+    str_actor2_path = 'actors_new.json'
     dict_actors_info_all = {}
     with codecs.open(str_actor1_path, 'r', 'utf-8') as f:
         dict_actors_info_all = json.load(f)
---
+++ python/movie_dir_regroup.py
@@ -0,0 +1,32 @@
+﻿# -*- coding:utf-8 -*-
+# Create on 20170801
+# Auth: wang.yijian
+# Desc: 电影目录处理
+
+
+import os
+import shutil
+
+def movie_dir_regroup(movie_path):
+    '''
+    DIR: Series/Series-No
+    '''
+    folders_all = os.listdir(movie_path)
+    for folder in folders_all:
+        dir_full = movie_path + '/' + folder
+        if os.path.isdir(dir_full) == False:
+            print(dir_full + ' is not a dir')
+            continue
+        sns = folder.split('-')
+        if len(sns) != 2:
+            print(folder + ' is not a series no')
+            continue
+        series = sns[0]
+        series_dir = movie_path + '/' + series
+        no = sns[1]
+        if os.path.exists(series_dir) == False:
+            os.mkdir(series_dir)
+        shutil.move(dir_full, series_dir)
+
+
+# movie_dir_regroup('F:\\SublimeText3\\Data\\d\\2\\1')
